7ba2f84c320d287310d6088bb399734ebf8f36e5
Bidane Gutiérrez Navas <Bidane@MacBook-Pro-de-Bidane.local>
2017-02-14 19:08:24 -0500

    week 6

---
+++ Week 6/sketch.js
@@ -0,0 +1,50 @@
+var x = 200;
+var y = 150;
+var xspeed = 15;
+var yspeed = 5;
+// balls
+numBalls = 2;
+var balls = new Array(2);
+
+function setup () {
+	createCanvas(1200, 600);
+	
+	for(i= 0 ; i < numBalls ; i++){
+	   balls[i] = new Ball(i*1, i*2);
+	}
+}
+
+function draw () {
+	background(20, 20, 30); 
+	for(i= 0 ; i < numBalls ; i++){
+	   balls[i].display();
+	   balls[i].move();
+	   balls[i].bounce();
+	}
+}
+
+function Ball(x, y) {
+	this.x = x;
+	this.y = y;
+
+	this.display = function() {
+		stroke(50);
+		strokeWeight(100);
+		fill(this.x, this.y, 200, 170, 250);
+		ellipse(this.x, this.y, 20, 20);
+	}
+	
+	this.move = function() {
+		this.x = this.x + xspeed 
+		this.y = this.y + yspeed 
+	}
+	
+	this.bounce = function() {
+		if (this.x > width || this.x < 0){
+			xspeed = xspeed *-1;
+		}
+		if (this.y > height || this.y < 0){
+			yspeed = yspeed *-1;
+		}
+	}
+}
